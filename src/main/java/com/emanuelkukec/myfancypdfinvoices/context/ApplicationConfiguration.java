8804b175ebdebbf6525c108ab756bb8608aa4cae
Emanuel Kukec <kukec.emanuel@gmail.com>
2025-09-07 12:21:44 +0200

    Completed lesson 5 Spring Database Access using Spring JDBC

--- src/main/java/com/emanuelkukec/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/emanuelkukec/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -2,26 +2,53 @@ package com.emanuelkukec.myfancypdfinvoices.context;
 
 import com.emanuelkukec.myfancypdfinvoices.ApplicationLauncher;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
 @EnableWebMvc
+@EnableTransactionManagement
 public class ApplicationConfiguration {
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setUrl("jdbc:h2:~/myFirstH2Database;INIT=RUNSCRIPT FROM 'classpath:/schema.sql'");
+        ds.setUser("sa");
+        ds.setPassword("sa");
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public TransactionManager platformTransactionManager(){
+        return new DataSourceTransactionManager(dataSource());
     }
 
     @Bean
